5c5b77aae32255d1d697b53196441170d4b815b1
KingHD <77498297+KingHD234@users.noreply.github.com>
2026-04-09 11:27:09 +0200

    Updated to working bullet bounce
    - bullets bounces as intended
     -except for a few minor bugs
    - cleaned up the bullet script again after totally wrecking it
    - just needs some MINOR bug fixes and Polishing

--- BangerProjekt/Assets/Scripts/CombatScripts/PlayerScripts/WeaponScripts/Bullet.cs
+++ BangerProjekt/Assets/Scripts/CombatScripts/PlayerScripts/WeaponScripts/Bullet.cs
@@ -9,23 +9,25 @@ public class PlayerBullet : MonoBehaviour
     protected Weapon weaponScript;
     protected Player playerScript;
     public Rigidbody2D rb;
-    public Vector2 pos;
-    public Vector2 poscol;
+    private int RemainingPierce;
+    private int RemainingBulletBounces;
+    public Vector2 bulletPos;
+    public bool isBouncing;
 
     private void Awake()
     {
         timeAlive = 20;
         weaponScript = GameObject.FindWithTag("Weapon").GetComponent<Weapon>();
         playerScript = GameObject.FindWithTag("Player").GetComponent<Player>();
+        RemainingBulletBounces = weaponScript.BulletBounces;
+        RemainingPierce = weaponScript.BulletPierce;
         rb = gameObject.GetComponent<Rigidbody2D>();
-        pos = gameObject.transform.position;
     }
 
     protected virtual void Start()
     {
         //THIS IS FOR TIMEALIVE AND DESTRUCTION
         StartCoroutine(BulletCountDown());
-        print(pos);
     }
     public IEnumerator BulletCountDown()
     {
@@ -51,6 +53,37 @@ public class PlayerBullet : MonoBehaviour
         }
         else return;
     }
+    public Vector2 BulletBounceCalculateForEnemies(Rigidbody2D _Rigidbody, GameObject currObject)
+    { // Like a smart man ones said "Einfallswinkel = Ausfallswinkel" but i never thought he meant some bs like this
+            StartCoroutine(IsBouncingcd());
+
+            Vector2 bulletvelocity = _Rigidbody.velocity; // getting the Velocity 
+            Vector2 closestPoint = currObject.GetComponent<CircleCollider2D>().ClosestPoint(bulletPos); // getting the exact collison point
+
+            Vector2 normal = (bulletPos - closestPoint).normalized; // getting a normalized vector of our collsion 
+            Vector2 newVelocity = Vector2.Reflect(bulletvelocity, normal); // Reflecting the bullet "mirroring" it on the normal
+
+            RemainingBulletBounces--; // now we remove a Bounce
+            return newVelocity;
+    }
+    public Vector2 BulletBounceCalculate(Rigidbody2D _Rigidbody, GameObject currObject)
+    { // Like a smart man ones said "Einfallswinkel = Ausfallswinkel" but i never thought he meant some bs like this
+        StartCoroutine(IsBouncingcd());
+
+        Vector2 bulletvelocity = _Rigidbody.velocity; // getting the Velocity 
+        Vector2 closestPoint = currObject.GetComponent<Collider2D>().ClosestPoint(bulletPos); // getting the exact collison point
+
+        Vector2 normal = (bulletPos - closestPoint).normalized; // getting a normalized vector of our collsion 
+        Vector2 newVelocity = Vector2.Reflect(bulletvelocity, normal); // Reflecting the bullet "mirroring" it on the normal
+
+        RemainingBulletBounces--; // now we remove a Bounce
+        return newVelocity;
+    }
+    public IEnumerator IsBouncingcd()
+    {
+        yield return new WaitForSeconds(0.001f);
+        isBouncing = false; 
+    }
     protected virtual void OnTriggerEnter2D(Collider2D collision)
     {
         GameObject currObject = collision.gameObject; //the object with which the collision occured
@@ -61,8 +94,8 @@ public class PlayerBullet : MonoBehaviour
             float CritDamage = CritCalculate();
             currObject.GetComponent<Enemy>().TakeDamage((int)((weaponScript.Damage * weaponScript.DamageMult) * CritDamage), CritDamage);
             LifeStealCalculate();
-            weaponScript.BulletPierce--; //reduce the pierce
-            if (weaponScript.BulletPierce <= 0) //and if there is no pierce left
+            RemainingPierce--; //reduce the pierce
+            if (RemainingPierce <= 0) //and if there is no pierce left
             {
                 currObject.GetComponent<Unit>().DamageUnit((int)(weaponScript.Damage * weaponScript.DamageMult));
                 RemainingPierce--; //reduce the pierce
@@ -71,26 +104,34 @@ public class PlayerBullet : MonoBehaviour
                     Destroy(gameObject); //and destroy the bullet
                 }
             }
-            if (weaponScript.BulletBounces > 0)
+            if (RemainingBulletBounces > 0 && !isBouncing)
             {
-                weaponScript.BulletBounces--;
+                isBouncing = true;
+                bulletPos = transform.position; // getting current bullet position
+
+                rb.velocity = BulletBounceCalculateForEnemies(rb, currObject); // calculating the new velocity
+                float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg - 90f; // Atan2 converts the velocity to an Angle in degrees // -90f because how the sprite is drawn
+                transform.rotation = Quaternion.Euler(0f, 0f, angle); // Here i apply the Angle to the Rotation Axis (Z) 
+
             }
         }
         else if (currObject.CompareTag("Wall") || currObject.CompareTag("Door")) //if the bullets collide with a wall
         {
-            if (weaponScript.BulletBounces <= 0)
+            if (RemainingBulletBounces <= 0) // checking for remaining bulletbounces 
             {
-                Destroy(gameObject); //destroy the bullet
+                Destroy(gameObject); //destroy the bullet if no bounce left
             }
-            poscol = currObject.transform.position;
-            print(poscol);
-            Vector2 newVelocity = Vector2.Reflect(pos, poscol.normalized);
-            print(newVelocity);
-            gameObject.transform.position *= newVelocity;
-            print(gameObject.transform.position);
-            print(pos);
-            
+
+            else if (!isBouncing)
+            {
+                isBouncing = true;
+                bulletPos = transform.position; // getting current bullet position
+
+                rb.velocity = BulletBounceCalculate(rb, currObject); // calculating the new velocity
+                float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg - 90f; // Atan2 converts the velocity to an Angle in degrees // -90f because how the sprite is drawn
+                transform.rotation = Quaternion.Euler(0f, 0f, angle); // Here i apply the Angle to the Rotation Axis (Z) 
+            }
         }
     }
-
 }
+// transform.forward
