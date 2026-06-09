2a26f34410d3d320c8551470dbc62b34f1b67ed6
Krzzt <Ferdinand.Knoblauch@gmail.com>
2026-06-09 12:23:21 +0200

    Emergency Culture Info commit

--- BangerProjekt/Assets/Scripts/CardScripts/CardEffects/CardEffect.cs
+++ BangerProjekt/Assets/Scripts/CardScripts/CardEffects/CardEffect.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 
 
 [CreateAssetMenu]
 public abstract class CardEffect : ScriptableObject
 {
-    public abstract void ExecuteEffect(string effect);
-    public abstract void RevertEffect(string effect);
+	public abstract void ExecuteEffect(string effect);
+	public abstract void RevertEffect(string effect);
+
+	protected CultureInfo info = new CultureInfo("en-EN");
 }
--- BangerProjekt/Assets/Scripts/CardScripts/CardEffects/PlayerCardEffects/AddBonusFireRateEffect.cs
+++ BangerProjekt/Assets/Scripts/CardScripts/CardEffects/PlayerCardEffects/AddBonusFireRateEffect.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "CardEffect/AddFireRateEffect")]
 public class AddBonusFireRateEffect : CardEffect
 {
 
-    float value;
+	float value;
 	public override void ExecuteEffect(string effect)
 	{
-		value = float.Parse(effect);
-        Player.Instance.BonusFireRate += value;
+		value = float.Parse(effect, info.NumberFormat);
+		Player.Instance.BonusFireRate += value;
 	}
 
 	public override void RevertEffect(string effect)
