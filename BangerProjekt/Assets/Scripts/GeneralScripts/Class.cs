a6ee9200e5bb14cb32e66ffc999542a7e094f6bb
Krzzt <Ferdinand.Knoblauch@gmail.com>
2026-06-10 15:14:19 +0200

    More Cleanup

--- BangerProjekt/Assets/Scripts/GeneralScripts/Class.cs
+++ BangerProjekt/Assets/Scripts/GeneralScripts/Class.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,13 @@ using UnityEngine;
 [CreateAssetMenu]
 public class Class : ScriptableObject
 {
-    [field: SerializeField] public string Name { get; set; }
-    [field:SerializeField] public Item[] StartingItems {get; set;}
-    [field: SerializeField] public List<Card> StartingDeck { get; set; }
-    [field:SerializeField] public float StartingBonusDamage { get; set;}
-    [field: SerializeField] public float StartingBonusFireRate { get; set; }
-    [field: SerializeField] public int StartingHealth{ get; set; }
-    [field: SerializeField] public int StartingMoveSpeed { get; set; }
-    [field: SerializeField] public Enums.Class CorrespondingEnum { get; set; }
-
-	[field:SerializeField] public float StartingBonusShotSpeed {  get; set; }
-	[field:SerializeField] public float StartingBonusMoveSpeed {  get; set; }
+	[field: SerializeField] public string Name { get; set; }
+	[field: SerializeField] public Item[] StartingItems { get; set; }
+	[field: SerializeField] public List<Card> StartingDeck { get; set; }
+	[field: SerializeField] public float StartingBonusDamage { get; set; }
+	[field: SerializeField] public float StartingBonusFireRate { get; set; }
+	[field: SerializeField] public int StartingHealth { get; set; }
+	[field: SerializeField] public int StartingMoveSpeed { get; set; }
+	[field: SerializeField] public float StartingBonusShotSpeed { get; set; }
+	[field: SerializeField] public float StartingBonusMoveSpeed { get; set; }
 }
